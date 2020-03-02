2601242682d453c145bdfc2737874406371ec70a
Wen Kokke <wenkokke@users.noreply.github.com>
2020-03-02 18:51:57 +0000

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,13 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
 setuptools.setup(
     name="sapphire",
     version="0.0.1",
     author="Wen Kokke",
     author_email="wen.kokke@ed.ac.uk",
     description="A library for translating TensorFlow models to Z3",
-    long_description=long_description,
+    license="Peer Production License",
+    long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/wenkokke/sapphire",
     packages=setuptools.find_packages(),
@@ -19,4 +17,5 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+    data_files = [("", ["LICENSE.txt"])]
 )
